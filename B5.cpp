45605c06f65e639f5ae0abb156ae1c6e8341571d
Aditya <109960864+KondeAditya@users.noreply.github.com>
2023-12-14 00:17:41 +0530

    Add files via upload

--- B5.cpp
+++ B5.cpp
@@ -1,41 +1,52 @@
 #include<iostream>
-#include<graphics.h>
 using namespace std;
-int move(int j,int h,int &x,int &y){
-    if(j==1){//jr aplyala vr jaycha asel tr y-h kartat karan graphics madhe inverted asta
-        y=y-h;
-    }else if(j==2){
-        x=x+h;
-    }else if(j==3){
-        y=y+h;
-    }else if(j==4){
-        x=x-h;
+int n;
+#define size 10
+template<class T>
+void sel(T A[size])
+{
+    int i,j,min;
+    T temp;
+    for(i=0;i<n-1;i++)
+    {
+        min=i;
+        for(j=i+1;j<n;j++)
+        {
+            if(A[j]<A[min])
+            min=j;
+        }
+        temp=A[i];
+        A[i]=A[min];
+        A[min]=temp;
     }
-    lineto(x,y);
-}
-void hilbert(int r,int d, int l,int u,int i,int h,int &x, int &y){
-    if(i>0){
-        i--;
-        hilbert(d,r,u,l,i,h,x,y);
-        move(r,h,x,y);//r
-        hilbert(r,d,l,u,i,h,x,y);
-        move(d,h,x,y);//d
-        hilbert(r,d,l,u,i,h,x,y);
-        move(l,h,x,y);//l
-        hilbert(u,l,d,r,i,h,x,y);
+    cout<<"\nSorted array:";
+    for(i=0;i<n;i++)
+    {
+        cout<<" "<<A[i];
     }
 }
-int main(){
-    int x0=50,y0=150,x,y,h=10,n,u=1,r=2,d=3,l=4;
-    x=x0;
-    y=y0;
-    cout<<"Enter n: ";
+
+int main()
+{
+    int A[size];
+    float B[size];
+    int i;
+   
+    cout<<"\nEnter total no of int elements:";
     cin>>n;
-    int gm,gd=DETECT;
-    initgraph(&gd,&gm,NULL);
-    moveto(x,y);
-    hilbert(r,d,l,u,n,h,x,y);
-    delay(5000);
-    getch();
-    return 0;
+    cout<<"\nEnter int elements:";
+    for(i=0;i<n;i++)
+    {
+        cin>>A[i];
+    }
+    sel(A);
+   
+    cout<<"\nEnter total no of float elements:";
+    cin>>n;
+    cout<<"\nEnter float elements:";
+    for(i=0;i<n;i++)
+    {
+        cin>>B[i];
+    }
+    sel(B);
 }
